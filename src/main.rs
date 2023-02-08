347641ddb5f0b7f5bfeb7019036773501a3ace4b
AndyLu <laserlue@gmail.com>
2023-02-08 16:00:50 +0800

    feat: add query_type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod byte_packet_buffer;
 mod dns_header;
+mod query_type;
 mod result_code;
 
 fn main() {
---
+++ src/query_type.rs
@@ -0,0 +1,22 @@
+/// Record type in the question
+#[derive(PartialEq, Eq, Debug, Clone, Hash, Copy)]
+pub enum QueryType {
+    UNKNOWN(u16),
+    A, // 1
+}
+
+impl QueryType {
+    pub fn to_num(&self) -> u16 {
+        match *self {
+            QueryType::UNKNOWN(x) => x,
+            QueryType::A => 1,
+        }
+    }
+
+    pub fn from_num(num: u16) -> Self {
+        match num {
+            1 => QueryType::A,
+            _ => QueryType::UNKNOWN(num),
+        }
+    }
+}
