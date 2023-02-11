d7be9f9bbcb62183545b5ce7e8ccf178c41208bf
AndyLu <laserlue@gmail.com>
2023-02-11 23:54:03 +0800

    docs: add comments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use byte_packet_buffer::BytePacketBuffer;
 use model::{dns_packet::DnsPacket, dns_question::DnsQuestion, query_type::QueryType};
-use std::{fs::File, io::Read, net::UdpSocket};
+use std::net::UdpSocket;
 mod byte_packet_buffer;
 mod model;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // A query for google.com
     let qname = "google.com";
     let qtype = QueryType::A;
 
+    // Using google's public DNS server
+    // ref: https://zh.wikipedia.org/zh-tw/Google_Public_DNS
     let server = ("8.8.8.8", 53);
 
+    // Bind a UDP socket to an arbitrary port
     let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
+    // Build our query packet. It's important that we remember to set the
+    // `recursion_desired` flag. As noted earlier, the packet id is arbitrary.
     let mut packet = DnsPacket::new();
 
     packet.header.id = 1234;
@@ -24,8 +30,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut req_buffer = BytePacketBuffer::new();
     packet.write(&mut req_buffer)?;
 
+    // Send it off to the server by using our socket
     socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
+
     let mut res_buffer = BytePacketBuffer::new();
+
+    // Ask the socket to write the response directly to our res_buffer
     socket.recv(&mut res_buffer.buf)?;
 
     let res_packet = DnsPacket::from_buffer(&mut res_buffer)?;
